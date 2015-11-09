9a8ae310f0bd1b46051c88262a538a6c112a9d13
billy <qweeah@sjtu.edu.cn>
2015-11-09 14:19:27 +0800

    test lseek *3, TODO:DATA&HOLE

---
+++ lseek.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+#define FILE_PATH "/home/billy/hmfsMount/a"
+
+/*
+ *     0        SEEK_SET
+ *     1        SEEK_CUR
+ *     2        SEEK_END
+ */
+
+int main(void)
+{
+	int whence = SEEK_SET-1;
+	FILE *file;
+	int fd;
+	off_t res, ofs;
+
+	file = fopen(FILE_PATH, "w");
+	printf("user file:%p\n", file);
+	fd = fileno(file);
+	if (fd) {
+		ofs = 1;
+		while (whence++ < SEEK_END) {
+			res = lseek(fd, ofs, whence);
+			printf("res:%d\n", (int)res);
+			ofs++;
+		}
+	}
+	return 0;
+}
